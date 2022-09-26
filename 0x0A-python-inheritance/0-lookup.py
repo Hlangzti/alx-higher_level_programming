6aaeffcb9abf97c9e3a0a175c7d0d74c851336a5
Hlangzti <hlangzti@gmail.com>
2022-09-26 03:07:53 -0700

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Author: Siyo"""
+
+
+def lookup(obj):
+    """A function that returns the list of available
+    attributes and methods of an object
+    Returns a list object
+    """
+    return dir(obj)
