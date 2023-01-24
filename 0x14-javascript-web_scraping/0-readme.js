6869fd3f61767368a763ec6c0ef15769f9f0313d
Hlangzti <Hlangzti@gmail.com>
2023-01-24 05:19:29 -0800

    task0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,13 @@
 #!/usr/bin/node
-const fs = require('fs');
-const args = process.argv;
-fs.readFile(args[2], (err, data) => {
-  if (err) console.log(err);
-  else console.log(data.toString());
+/* Opens a file and prints its contents, else print error */
+
+const filename = process.argv[2];
+const { readFile } = require('fs');
+
+readFile(filename, 'utf-8', (error, text) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(text);
 });
