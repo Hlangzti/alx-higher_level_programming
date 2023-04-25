3fc3c971f7d5b0f2c848fdf219bf43b9b38234fd
Hlangalwandile Siyo <104855443+Hlangzti@users.noreply.github.com>
2023-04-25 13:49:41 +0200

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-const args = process.argv;
-const request = require('request');
-request(args[2], function (error, response, body) {
-  if (error === null) console.log('code:', response.statusCode);
-});
