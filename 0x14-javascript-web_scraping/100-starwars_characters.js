0c195b45a11d8386837a1c7d27214f0ccb9fc611
Hlangzti <Hlangzti@gmail.com>
2023-04-26 11:35:09 +0000

    task7-8

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,13 +1,21 @@
-t request = require('request');
-const args = process.argv;
-const url = 'https://swapi-api.hbtn.io/api/films/' + args[2];
+#!/usr/bin/node
+const request = require('request');
+
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 request(url, function (error, response, body) {
-  if (error === null) {
-    const characters = JSON.parse(body).characters;
-    for (const idx in characters) {
-      request(characters[idx], function (error, response, body) {
-        if (error === null) console.log(JSON.parse(response.body).name);
-      });
-    }
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const data = JSON.parse(body);
+  for (const character of data.characters) {
+    request(character, function (error, response, body) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      const cast = JSON.parse(body);
+      console.log(`${cast.name}`);
+    });
   }
 });
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,31 @@
-t request = require('request');
-const args = process.argv;
-const url = 'https://swapi-api.hbtn.io/api/films/' + args[2];
+#!/usr/bin/node
+const request = require('request');
+
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
 request(url, async function (error, response, body) {
-  if (error === null) printer(JSON.parse(body).characters, 0);
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const data = JSON.parse(body);
+  for (const url of data.characters) {
+    try {
+      const cast = await getCharacter(url);
+      console.log(cast.name);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 });
 
-function printer (characters, i) {
-  request(characters[i], function (error, response, body) {
-    if (error === null) {
-      console.log(JSON.parse(response.body).name);
-      if (i + 1 < characters.length) printer(characters, i + 1);
-    }
+function getCharacter (url) {
+  return new Promise((resolve, reject) => {
+    request(url, function (error, response, body) {
+      if (error) {
+        reject(error);
+        return;
+      }
+      resolve(JSON.parse(body));
+    });
   });
 }
